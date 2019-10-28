c005bfe32fedb65e916793bac7015093fedc2171
longnhong <longnhong23@gmail.com>
2019-10-28 18:14:35 +0700

    add

--- web/method.go
+++ web/method.go
@@ -18,7 +18,7 @@ func ResParamArrUrlClient(url string, objArray interface{}, objRes interface{}) 
 	resp, err := client.Do(req)
 	if err != nil {
 		logMarshal.Errorf(url, "error marshal post")
-		panic(err)
+		return err
 	}
 	defer resp.Body.Close()
 	return json.NewDecoder(resp.Body).Decode(&objRes)
@@ -29,7 +29,7 @@ func ResUrlClientGet(url string, objRes interface{}) error {
 	resp, err := http.Get(url)
 	if err != nil {
 		logMarshal.Errorf(url, "error marshal post")
-		panic(err)
+		return err
 	}
 	defer resp.Body.Close()
 	return json.NewDecoder(resp.Body).Decode(&objRes)
