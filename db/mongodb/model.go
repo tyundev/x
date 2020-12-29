2ee5785ac7c3d52c21fa7627ddebe4214df727ba
GAIIT <phamthucuc95@gmail.com>
2020-12-29 09:52:49 +0700

    add omitempty created_at khi update

--- db/mongodb/model.go
+++ db/mongodb/model.go
@@ -12,8 +12,8 @@ type IModel interface {
 }
 type BaseModel struct {
 	ID        string `json:"id" bson:"_id"`
-	CreatedAt int64  `json:"created_at" bson:"created_at"`
-	UpdatedAt int64  `json:"updated_at" bson:"updated_at"`
+	CreatedAt int64  `json:"created_at,omitempty" bson:"created_at"`
+	UpdatedAt int64  `json:"updated_at,omitempty" bson:"updated_at"`
 	DeletedAt int64  `json:"deleted_at" bson:"deleted_at"`
 }
 
