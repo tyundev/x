b5555ae78457b5e3e302e750670f74efe5c1a992
GAIIT <phamthucuc95@gmail.com>
2021-09-10 01:34:59 +0700

    fix password

--- utils/auth/password.go
+++ utils/auth/password.go
@@ -3,7 +3,7 @@ package auth
 import "golang.org/x/crypto/bcrypt"
 
 func GererateHashedPassword(p string) (string, error) {
-	hashed, err := bcrypt.GenerateFromPassword([]byte(p), 11)
+	hashed, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.DefaultCost)
 	return string(hashed), err
 }
 
